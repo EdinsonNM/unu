fad295eea9031051e44ee53faa46101d1d53dd2e
Luis <luchopintado@gmail.com>
2015-07-20 08:09:09 -0500

    Agregada la paginacion para revision (Aula)

--- controllers/AulaController.js
+++ controllers/AulaController.js
@@ -6,15 +6,24 @@ module.exports=function(){
       var controller=baucis.rest('Aula');
       controller.relations(true);
 
-      controller.get('/methods/paginate', function(req, res){
+      controller.get('/methods/paginate', function(req, res){        
       	var limit = req.query.limit;
-      	var page = req.query.page;
+      	var page = req.query.page || 1;
       	var filters = req.query.filters;
       	model.paginate(
       		filters, 
       		{page: page, limit: limit}, 
       		function(err, results, pageCount, itemCount){
-      			res.send(results);
+            var obj = {
+              total: itemCount,
+              perpage: limit*1,
+              current_page: page*1,
+              last_page: Math.ceil(itemCount/limit),
+              from: (page-1)*pageCount+1,
+              to: page*pageCount,
+              data: results
+            };
+      			res.send(obj);
       		}
       	);
       });
