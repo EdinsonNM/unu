f21cc298cee1323a1d871c837f2236687dd1cca9
Edinson Nuñez More <nmedinson@gmail.com>
2015-08-20 01:07:21 -0500

    Agregar paginación a plan de estudios

--- controllers/PlanestudioController.js
+++ controllers/PlanestudioController.js
@@ -6,6 +6,31 @@ module.exports=function(){
       var controller=baucis.rest('Planestudio');
       controller.relations(true);
       controller.hints(true);
+      controller.fragment('/planestudios');
+      //custom methods
+
+      controller.get('/methods/paginate', function(req, res){
+      	var limit = req.query.count;
+      	var page = req.query.page || 1;
+      	var filter = req.query.filter;
+      	model.paginate(
+      		filter,
+      		{page: page, limit: limit},
+      		function(err, results, pageCount, itemCount){
+            var obj = {
+              total: itemCount,
+              perpage: limit*1,
+              current_page: page*1,
+              last_page: Math.ceil(itemCount/limit),
+              from: (page-1)*pageCount+1,
+              to: page*pageCount,
+              data: results
+            };
+      			res.send(obj);
+      		}
+      	);
+      });
+
     }
   }
 }
