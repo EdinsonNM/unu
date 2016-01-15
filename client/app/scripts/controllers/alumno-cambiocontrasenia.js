5d4e5bc98e54a675ae2e132068aa5381ecc3b56f
Jalcar <jalcar24@gmail.com>
2016-01-15 00:19:08 -0500

    terminando el cambio de contraseña Alumnos

--- client/app/scripts/controllers/alumno-cambiocontrasenia.js
+++ client/app/scripts/controllers/alumno-cambiocontrasenia.js
@@ -1,22 +1,22 @@
 (function() {
   'use strict';
   angular.module('unuApp')
-  .controller('AlumnoCambioContraseniaCtrl',['$scope','Restangular','$rootScope',function($scope,Restangular,$rootScope){
+  .controller('AlumnoCambioContraseniaCtrl',['$scope','Restangular','$rootScope', 'ToastMD', 'MessageFactory',function($scope,Restangular,$rootScope,ToastMD,MessageFactory){
+    var service = Restangular.all('usuarios');
     $scope.UI = {
-      title:'Cambio Contraseña'
+      title:'Cambio Contraseña',
+      message: MessageFactory
     };
     $rootScope.$watch('ALUMNO',function(newValue,oldValue){
         $scope.model = Restangular.copy(newValue);
     },true);
 
-    /*$scope.Edit = function Edit($event) {
-      $scope.UI.editMode = true;
-      console.log('edit');
-    };*/
-
     $scope.Save = function Save($event) {
-      $scope.model.save().then(function(result){
-        console.log(result);
+      service.customPUT($scope.model,'auth/change-password').then(function(res){
+        ToastMD.success('Contraseña cambiada correctamente.');
+      },
+      function(res){
+        ToastMD.warning(res.data.message);
       });
     };
 
