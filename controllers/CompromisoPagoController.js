1a2e1ff149ff72d01dd469c065a5e065bcf93ddb
hencarpa <hencarpa@gmail.com>
2016-02-28 14:23:07 -0500

    Controler conflictosalumno y compromisoPago

--- controllers/CompromisoPagoController.js
+++ controllers/CompromisoPagoController.js
@@ -1,9 +1,9 @@
-var model = require('../models/ConflictosAlumnoModel.js');
+var model = require('../models/CompromisoPagoModel.js');
 module.exports=function(){
   var baucis=require('baucis');
   return{
     setup:function(){
-      var controller=baucis.rest('ConflictosAlumno');
+      var controller=baucis.rest('CompromisoPago');
       controller.relations(true);
       controller.hints(true);
     }
