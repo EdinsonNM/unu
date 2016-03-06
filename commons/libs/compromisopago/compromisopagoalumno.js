08a0a4ffa6788c92cf345ff5124214342aa1ce83
Yoel Jimenez Cespedez <yojicp@gmail.com>
2016-03-06 09:17:10 -0500

    Avance compromiso de pagos individual

--- commons/libs/compromisopago/compromisopagoalumno.js
+++ commons/libs/compromisopago/compromisopagoalumno.js
@@ -13,13 +13,32 @@ const TASA={
   PAGO_ORDINARIO:'01',
   PAGO_RECARGO_EXTEMPORANEO:'02',
   PAGO_REPETICION_CURSO:'03',
-  PERDIDA_GRATUIDAD:{
-    OBSERVADO:''
-  }
+  PERDIDA_GRATUIDAD:'04', //TODO se esta asumiendo que existe una tasa para este pago, agregarlo a la BD y actualizar el codigo
+  PAGO_CARNET_UNIVERSITARIO:'05'//TODO se esta asumiendo que existe una tasa para este pago, agregarlo a la BD y actualizar el codigo
 };
 const PROCESO={
   ORDINARIO:'09',
   EXTRAORDINARIO:''
+};
+const MODALIDAD_INGRESO={
+  SEGUNDA_CARRERA:'01' //TODO Actualizar este codigo de acuerdo a la base de datos.
+};
+const SITUACION={
+  INGRESANTE: '01',
+  REGULAR: '02',
+  DESAPROBADO: '03',
+  SIN_RETIRO: '04',
+  CREDITOS_MENOR_PERMITIDO:'05',
+  AMONESTADO:'06',
+  REZAGADO:'07',
+  CON_SUSPENSION:'08',
+  OBSERVADO:'09',
+  PRIMER_PUESTO:'10',
+  REACTUALIZACION_MATRICULA:'11',
+  DEPORTISTA_INACTIVO:'12'
+};
+const PARAMETROS={
+  CREDITOS_MENOR_PERMITIDO:12
 };
 class CompromisoPagoAlumno{
   constructor(matriculaId){
@@ -36,9 +55,15 @@ class CompromisoPagoAlumno{
   ObtenerMatricula(){
     var self = this;
     var defer = Q.defer();
-    Matricula.finOne({_id:this.matriculaId})
+    Matricula.finOne({_id:self.matriculaId})
     .populate([
-      {path:'_alumno'},
+      {path:'_alumno',
+        populate:[
+                {path:'_tipoCondicionAlumno',model:'TipoCondicionAlumno'},
+                {path:'_ingresante',model:'Ingresante',
+                  populate:{path:'_modalidad',model: 'ModalidadIngreso'}
+                }
+      ]},
       {path:'_detalleMatricula'},
       {path:'_periodo',populate:{path:'procesos._proceso',model:'Proceso'}}
     ])
@@ -62,46 +87,68 @@ class CompromisoPagoAlumno{
     return defer.promise;
   }
   ObtenerDeudaMatriculaOrdinaria(){
-    let tasa = _findWhere(this.tasas,{codigo:TASA.PAGO_ORDINARIO});
+    let tasa = _.findWhere(this.tasas,{codigo:TASA.PAGO_ORDINARIO});
     this.deudas.push(tasa);
     return tasa;
   }
-  ObtenerDeudaPensionEnzenianza(){
-    //TODO implementar proceso, si tiene deuda por este conceprto entonces this.deudas.push(tasa);
-  }
   ObtenerDeudaPerdidaGratuidad(){
-    //TODO implementar proceso de verificacion y agregar deuda si corresponde
-  }
-  ObtenerDeudaPerdidaGratuidad(){
-    //TODO implementar proceso de verificacion y agregar deuda si corresponde
-  }
-  ObtenerDeudaPerdidaGratuidad(){
-    //TODO implementar proceso de verificacion y agregar deuda si corresponde
+    var self = this;
+    let modalidad = self.matricula._alumno._ingresante_modalidad.codigo;
+    let condicion = self.matricula._alumno._tipoCondicionAlumno.codigo;
+    let tasa = _.findWhere(self.tasas,{codigo:TASA.PERDIDA_GRATUIDAD});
+    if(modalidad == MODALIDAD_INGRESO.SEGUNDA_CARRERA){
+      self.deudas.push(tasa);
+    }else if(self.matricula.totalCreditos < PARAMETROS.CREDITOS_MENOR_PERMITIDO){
+      self.deudas.push(tasa);
+    }else if (1){  //NOTE DeudaCursosRepetidos preguntar si tiene cursos repetidos.
+      self.deudas.push(tasa);
+    }else if (1){  //NOTE ponderado semestral menor a 10.5
+      self.deudas.push(tasa);
+    }else{
+      //TODO Consultar si las tasas son diferentes por observado, rezagado, y los demas estados....
+      switch(condicion){
+        case SITUACION.OBSERVADO:
+        case SITUACION.REZAGADO:
+        case SITUACION.DESAPROBADO:
+        case SITUACION.SIN_RETIRO:
+        case SITUACION.CREDITOS_MENOR_PERMITIDO:
+        case SITUACION.AMONESTADO:
+        case SITUACION.CON_SUSPENSION:
+        case SITUACION.REACTUALIZACION_MATRICULA:  //TODO CONSULTAR LA LOGICA DE NEGOCIO Y COMPLETAR LA FUNCION DE SER NECESARIO.
+        case SITUACION.DEPORTISTA_INACTIVO:
+        case SITUACION.OBSERVADO: self.deudas.push(tasa); break;
+      }
+    }
+    return tasa;
   }
   ObtenerDeudaCarnetUniversitario(){
-    //TODO implementar proceso de verificacion y agregar deuda si corresponde
+    let tasa = _.findWhere(this.tasas,{codigo:TASA.PAGO_CARNET_UNIVERSITARIO});
+    this.deudas.push(tasa);
+    return tasa;
+    //TODO DETERMINAR EN EL MODELO COMO SE CONTROL PARA EL COBRO DE CARNET DADO QUE ES ANUAL y APLICAR EL FILTRO.
   }
   ObtenerDeudaReactualizacionMatricula(){
+    //TODO implementar proceso de verificacion y agregar deuda si corresponde
+  }
+  ObtenerRecargoExtemporanea(){
     //TODO implementar proceso de verificacion y agregar deuda si corresponde
   }
   generar(next){
     var self = this;
     Q
-    .fcall(ObtenerMatricula)
-    .then(ObtenerTasas)
-    .then(ObtenerDeudaMatriculaOrdinaria)
-    .then(ObtenerDeudaPensionEnzenianza)
-    .then(ObtenerDeudaPerdidaGratuidad)
-    .then(ObtenerDeudaReactualizacionMatricula)
-    .then(ObtenerDeudaCursosRepetidos)
-    .then(ObtenerRecargoExtemporanea)
+    .fcall(self.ObtenerMatricula())
+    .then(self.ObtenerTasas())
+    .then(self.ObtenerDeudaMatriculaOrdinaria())
+    .then(self.ObtenerDeudaPerdidaGratuidad())
+    .then(self.ObtenerDeudaCarnetUniversitario())
+    .then(self.ObtenerRecargoExtemporanea())
     .then(function(){
       //TODO this.deudas contiene todos los montos que se han añadido para la cobranza de la matricula
       //TODO se debe generar el compromiso de pago
       var compromisopago = new CompromisoPago();
       compromisopago.save(function(err,compromiso){
-        return next(null,compromiso)
-      })
+        return next(null,compromiso);
+      });
 
     });
 
