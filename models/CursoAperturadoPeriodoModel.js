0c244fcc9619557c9f1cfdf18f77a7e078ab4984
Edinson Nuñez More <nmedinson@gmail.com>
2016-03-16 13:02:48 -0500

    Se renombro archivo

---
+++ models/CursoAperturadoPeriodoModel.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose');
+var Schema   = mongoose.Schema;
+var mongoosePaginate = require('mongoose-paginate');
+var uniqueValidator = require('mongoose-unique-validator');
+
+var CursoAperturadoPeriodoSchema = new Schema({
+  _periodo:{
+    type:Schema.Types.ObjectId,
+    ref:'Periodo',
+    required:true
+  },
+  _planestudiodetalle:{
+    type:Schema.Types.ObjectId,
+    ref:'Planestudiodetalle',
+    required:true
+  },
+  _grupos:[{
+    type: Schema.Types.ObjectId,
+    ref: 'GrupoCurso',
+    required: true
+  }],
+  createdAt:Date,
+	updatedAt:Date
+});
+
+CursoAperturadoPeriodoSchema.plugin(mongoosePaginate);
+CursoAperturadoPeriodoSchema.plugin(uniqueValidator);
+CursoAperturadoPeriodoSchema.pre('save',function(next){
+	var now = new Date();
+	this.updatedAt = now;
+	if (!this.createdAt){
+		this.createdAt=now;
+	}
+	next();
+});
+
+module.exports = mongoose.model('CursoAperturadoPeriodo', CursoAperturadoPeriodoSchema).plural('cursoaperturadoperiodos');
